6d2d5abd02295549a9c68ac15444eac4c9ea3708
Alan Jui <alanjui.1960@gmail.com>
2016-06-25 21:43:47 +0800

    Class Basic

--- src/app.js
+++ src/app.js
@@ -1,8 +1,32 @@
 class Drone {
-  
+  constructor(id, name) {
+    this._id = id;
+    this._name = name;
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  set id(value) {
+    this._id = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  static getCompany() {
+    console.log(`id: ${this.id}`);
+  }
+
+  fly() {
+    console.log(`Drone ${this.id} is flying`);
+  }
 }
 
-let drone = new Drone();
+let drone = new Drone('A123', 'flyer');
+drone.id = 'B456';
 
-console.log(`drone instanceof Drone: ${drone instanceof Drone}`);
+console.log(`Drone - ${drone.id} : ${drone.name}`);
 
